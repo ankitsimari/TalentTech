a2a9c2407a467691a237b1655cd650ee3859213d
Ankit Kumar <ankkrs@gmail.com>
2023-11-10 19:23:51 +0530

    initial setup

---
+++ Frondend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3 className='text-red-700 text-center mt-5 font-bold' >AI-Interview</h3>
+    </div>
+  );
+}
+
+export default App;
