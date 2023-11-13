a2679f74ce2724d6f860fe26c12b97ca5032ad37
Ankit Kumar <ankkrs@gmail.com>
2023-11-13 19:36:53 +0530

    chats updated

--- Frontend/src/pages/Analytics.jsx
+++ Frontend/src/pages/Analytics.jsx
@@ -7,59 +7,63 @@ import PolarChart from "../component/Chart/PolarChart";
 const Analytics = () => {
   const [chart,setChart] = useState("bar")
   const name = ["Subject", "Communication", "Interview"];
-  const colors = ["#7a3ff3", "black", "rgb(247,190,56)"];
+  // const colors = ["#7a3ff3", "black", "rgb(247,190,56)"];
   const Length = [9, 5, 7];
 
-  const [userData, setUserData] = useState({
-    labels: name,
-    datasets: [
-      {
-        label: "Skills",
-        data: Length,
-        backgroundColor: colors,
-        borderColor: "black",
-        borderWidth: 2,
-      },
-    ],
-  });
+  // const [userData, setUserData] = useState({
+  //   labels: name,
+  //   datasets: [
+  //     {
+  //       label: "Skills",
+  //       data: Length,
+  //       backgroundColor: colors,
+  //       borderColor: "black",
+  //       borderWidth: 2,
+  //     },
+  //   ],
+  // });
 
+  const colors = {
+    Subject: "#7a3ff3",
+    Communication: "black",
+    Interview: "rgb(247,190,56)",
+  };
+  
   const data = {
     labels: ["Interview 1", "Interview 2", "Interview 3"],
     datasets: [
       {
         label: "Subject",
         data: [10, 20, 15],
-        backgroundColor: colors,
+        backgroundColor: colors.Subject,
         borderWidth: 2,
         fill: false,
       },
       {
         label: "Communication",
         data: [5, 15, 10],
-        backgroundColor: colors,
+        backgroundColor: colors.Communication,
         borderWidth: 2,
         fill: false,
       },
       {
-        label: "Communication",
+        label: "Interview",
         data: [8, 12, 11],
-        backgroundColor: colors,
+        backgroundColor: colors.Interview,
         borderWidth: 2,
         fill: false,
-      }
-
+      },
     ],
   };
-
   return (
-    <div>
+    <div >
 
       <select value={chart} className="ms-8 px-3 py-2 border-2 font-bold border-solid text-customColor border-customColor" onChange={(e)=>setChart(e.target.value)}>
         <option value="line">Line</option>
         <option value="bar">Bar</option>
       </select>
         
-      <div className="">
+      <div className="flex justify-center mt-8">
           <div
             className="shadow p-4"
             style={{ width: "80%" }}
