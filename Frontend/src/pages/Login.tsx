f7709a9e304a44ba2575bbad7a9fbfa946b306a0
unknown <makarand.gijam22@gmail.com>
2023-11-13 17:43:01 +0530

    updated

--- Frontend/src/pages/Login.tsx
+++ Frontend/src/pages/Login.tsx
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import axios from "axios";
-// import "../styles/Login.css";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { setAuthCookies } from "../utils/cookie";
 
 export const Login: React.FC = () => {
   const [username, setUsername] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPass] = useState<string>("");
   const [isSignUp, setIsSignUp] = useState<boolean>(false);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const url = `http://localhost:7700`;
 
   const handleSignUp = () => {
     setIsSignUp(true);
@@ -26,12 +27,15 @@ export const Login: React.FC = () => {
     console.log("login-payload", payload);
 
     axios
-      .post("http://localhost:7700/auth/login", payload)
+      .post(`${url}/auth/login`, payload)
       .then((res) => {
         console.log(res.data);
+        const token = res.data.token;
+        const username = res.data.user.username;
+        setAuthCookies(token, username);
         localStorage.setItem("token", res.data.token);
         alert("Login Successful");
-        navigate("/")
+        navigate("/");
       })
       .catch((err) => console.log(err));
 
@@ -46,12 +50,15 @@ export const Login: React.FC = () => {
     console.log("login-payload", payload);
 
     axios
-      .post("http://localhost:7700/auth/login", payload)
+      .post(`${url}/auth/login`, payload)
       .then((res) => {
         console.log(res.data);
+        const token = res.data.token;
+        const username = res.data.user.username;
+        setAuthCookies(token, username);
         localStorage.setItem("token", res.data.token);
         alert("Guest Login Successful");
-        navigate("/")
+        navigate("/");
       })
       .catch((err) => console.log(err));
 
@@ -71,11 +78,14 @@ export const Login: React.FC = () => {
     console.log("signup-payload", payload);
 
     axios
-      .post("http://localhost:7700/auth/register", payload)
+      .post(`${url}/auth/register`, payload)
       .then((res) => {
         console.log(res.data);
+        const token = res.data.token;
+        const username = res.data.user.username;
+        setAuthCookies(token, username);
         alert("User Registered Successfully");
-        navigate("/")
+        navigate("/");
       })
       .catch((err) => console.log(err));
     setIsSignUp(true);
@@ -90,29 +100,29 @@ export const Login: React.FC = () => {
       <div className={`container ${isSignUp ? "right-panel-active" : ""}`}>
         <div className="form-container sign-up-container">
           <form onSubmit={handleSignUpClick} className="text-center">
-            <h1>Create Account</h1>
+            <h1 className="font-bold m-0">Create Account</h1>
             <input
               type="text"
               placeholder="Name"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
-              className="input-field"
+              className="bg-eee border border-solid border-gray-300 px-4 py-3 my-2 w-full"
             />
             <input
               type="email"
               placeholder="Email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="input-field"
+              className="bg-eee border border-solid border-gray-300 px-4 py-3 my-2 w-full"
             />
             <input
               type="password"
               placeholder="Password"
               value={password}
               onChange={(e) => setPass(e.target.value)}
-              className="input-field"
+              className="bg-eee border border-solid border-gray-300 px-4 py-3 my-2 w-full"
             />
-            <button type="submit" className="button1">
+            <button type="submit" className="border-2 border-solid border-purple-600 bg-purple-600 text-white text-sm font-bold uppercase py-3 px-10 rounded-full transition-transform duration-80 ease-in focus:outline-none active:scale-95">
               Sign Up
             </button>
           </form>
@@ -120,27 +130,27 @@ export const Login: React.FC = () => {
 
         <div className="form-container sign-in-container">
           <form onSubmit={handleSignInClick} className="text-center">
-            <h1>Sign in</h1>
+            <h1 className="font-bold m-0">Sign in</h1>
             <input
               type="email"
               placeholder="Email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="input-field"
+              className="bg-eee border border-solid border-gray-300 px-4 py-3 my-2 w-full"
             />
             <input
               type="password"
               placeholder="Password"
-              className="input-field"
+              className="bg-eee border border-solid border-gray-300 px-4 py-3 my-2 w-full"
               value={password}
               onChange={(e) => setPass(e.target.value)}
             />
-            <button type="submit" className="button1">
+            <button type="submit" className="border-2 border-solid border-purple-600 bg-purple-600 text-white text-sm font-bold uppercase py-3 px-10 rounded-full transition-transform duration-80 ease-in focus:outline-none active:scale-95">
               Sign In
             </button>
             <br />
-            <button onClick={handleGuestSignInClick} className="button1">
-              Continue as a guest
+            <button onClick={handleGuestSignInClick} className="border-2 border-solid border-purple-600 bg-purple-600 text-white text-sm font-bold uppercase py-3 px-10 rounded-full transition-transform duration-80 ease-in focus:outline-none active:scale-95">
+              Guest Login
             </button>
           </form>
         </div>
@@ -148,18 +158,18 @@ export const Login: React.FC = () => {
         <div className="overlay-container">
           <div className="overlay">
             <div className="overlay-panel overlay-left">
-              <h1>Welcome Back!</h1>
-              <p>
+              <h1 className="font-bold m-0">Welcome Back!</h1>
+              <p className="text-base font-light leading-5 tracking-wider my-5">
                 To keep connected with us please login with your personal info
               </p>
-              <button className="ghost" onClick={handleSignIn}>
+              <button className="border-2 border-solid border-white bg-purple-600 text-white text-sm font-bold uppercase py-3 px-10 rounded-full transition-transform duration-80 ease-in focus:outline-none active:scale-95" onClick={handleSignIn}>
                 Sign In
               </button>
             </div>
             <div className="overlay-panel overlay-right">
-              <h1>Hello, Explorer!</h1>
-              <p>Enter your personal details and start the journey with us</p>
-              <button className="ghost" onClick={handleSignUp}>
+              <h1 className="font-bold m-0">Hello, Explorer!</h1>
+              <p className="text-base font-light leading-5 tracking-wider my-5">Enter your personal details and start the journey with us</p>
+              <button className="border-2 border-solid border-white bg-purple-600 text-white text-sm font-bold uppercase py-3 px-10 rounded-full transition-transform duration-80 ease-in focus:outline-none active:scale-95" onClick={handleSignUp}>
                 Sign Up
               </button>
             </div>
@@ -170,196 +180,146 @@ export const Login: React.FC = () => {
   );
 };
 
-const DIV = styled.div `
+const DIV = styled.div`
   * {
-  box-sizing: border-box;
-}
+    box-sizing: border-box;
+    margin: auto
+  }
 
-h1 {
-  font-weight: bold;
-  margin: 0;
-}
+  form {
+    background-color: #ffffff;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    padding: 0 50px;
+    height: 100%;
+    text-align: center;
+  }
 
-h2 {
-  text-align: center;
-}
+  .container {
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+    position: relative;
+    overflow: hidden;
+    width: 768px;
+    max-width: 100%;
+    min-height: 480px;
+  }
 
-p {
-  font-size: 16px;
-  font-weight: 100;
-  line-height: 20px;
-  letter-spacing: 0.5px;
-  margin: 20px 0 30px;
-}
+  .form-container {
+    position: absolute;
+    top: 0;
+    height: 100%;
+    transition: all 0.6s ease-in-out;
+  }
 
-button {
-  border-radius: 20px;
-  border: 1px solid #7a3ff3;
-  background-color: #7a3ff3;
-  color: white;
-  font-size: 12px;
-  font-weight: bold;
-  padding: 12px 45px;
-  letter-spacing: 1px;
-  text-transform: uppercase;
-  transition: transform 80ms ease-in;
-}
+  .sign-in-container {
+    left: 0;
+    width: 50%;
+    z-index: 2;
+  }
 
-button:active {
-  transform: scale(0.95);
-}
+  .container.right-panel-active .sign-in-container {
+    transform: translateX(100%);
+  }
 
-button:focus {
-  outline: none;
-}
-
-button.ghost {
-  background-color: transparent;
-  border-color: #ffffff;
-}
-
-form {
-  background-color: #ffffff;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  padding: 0 50px;
-  height: 100%;
-  text-align: center;
-}
-
-input {
-  background-color: #eee;
-  border: none;
-  padding: 12px 15px;
-  margin: 8px 0;
-  width: 100%;
-}
-
-.container {
-  background-color: #fff;
-  border-radius: 10px;
-  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
-  position: relative;
-  overflow: hidden;
-  width: 768px;
-  max-width: 100%;
-  min-height: 480px;
-}
-
-.form-container {
-  position: absolute;
-  top: 0;
-  height: 100%;
-  transition: all 0.6s ease-in-out;
-}
-
-.sign-in-container {
-  left: 0;
-  width: 50%;
-  z-index: 2;
-}
-
-.container.right-panel-active .sign-in-container {
-  transform: translateX(100%);
-}
-
-.sign-up-container {
-  left: 0;
-  width: 50%;
-  opacity: 0;
-  z-index: 1;
-}
-
-.container.right-panel-active .sign-up-container {
-  transform: translateX(100%);
-  opacity: 1;
-  z-index: 5;
-  animation: show 0.6s;
-}
-
-@keyframes show {
-  0%,
-  49.99% {
+  .sign-up-container {
+    left: 0;
+    width: 50%;
     opacity: 0;
     z-index: 1;
   }
 
-  50%,
-  100% {
+  .container.right-panel-active .sign-up-container {
+    transform: translateX(100%);
     opacity: 1;
     z-index: 5;
+    animation: show 0.6s;
   }
-}
 
-.overlay-container {
-  position: absolute;
-  top: 0;
-  left: 50%;
-  width: 50%;
-  height: 100%;
-  overflow: hidden;
-  transition: transform 0.6s ease-in-out;
-  z-index: 100;
-}
+  @keyframes show {
+    0%,
+    49.99% {
+      opacity: 0;
+      z-index: 1;
+    }
 
-.container.right-panel-active .overlay-container {
-  transform: translateX(-100%);
-}
+    50%,
+    100% {
+      opacity: 1;
+      z-index: 5;
+    }
+  }
 
-.overlay {
-  background: #7a3ff3;
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: 0 0;
-  color: #ffffff;
-  position: relative;
-  left: -100%;
-  height: 100%;
-  width: 200%;
-  transform: translateX(0);
-  transition: transform 0.6s ease-in-out;
-}
+  .overlay-container {
+    position: absolute;
+    top: 0;
+    left: 50%;
+    width: 50%;
+    height: 100%;
+    overflow: hidden;
+    transition: transform 0.6s ease-in-out;
+    z-index: 100;
+  }
 
-.container.right-panel-active .overlay {
-  transform: translateX(50%);
-}
+  .container.right-panel-active .overlay-container {
+    transform: translateX(-100%);
+  }
 
-.overlay-panel {
-  position: absolute;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  padding: 0 40px;
-  text-align: center;
-  top: 0;
-  height: 100%;
-  width: 50%;
-  transform: translateX(0);
-  transition: transform 0.6s ease-in-out;
-}
+  .overlay {
+    background: #7a3ff3;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: 0 0;
+    color: #ffffff;
+    position: relative;
+    left: -100%;
+    height: 100%;
+    width: 200%;
+    transform: translateX(0);
+    transition: transform 0.6s ease-in-out;
+  }
 
-.overlay-left {
-  transform: translateX(-20%);
-}
+  .container.right-panel-active .overlay {
+    transform: translateX(50%);
+  }
 
-.container.right-panel-active .overlay-left {
-  transform: translateX(0);
-}
+  .overlay-panel {
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    padding: 0 40px;
+    text-align: center;
+    top: 0;
+    height: 100%;
+    width: 50%;
+    transform: translateX(0);
+    transition: transform 0.6s ease-in-out;
+  }
 
-.overlay-right {
-  right: 0;
-  transform: translateX(0);
-}
+  .overlay-left {
+    transform: translateX(-20%);
+  }
 
-.container.right-panel-active .overlay-right {
-  transform: translateX(20%);
-}
+  .container.right-panel-active .overlay-left {
+    transform: translateX(0);
+  }
 
-.button1{
-  background-color: #7a3ff3;
-  color: white;
-}
-`
+  .overlay-right {
+    right: 0;
+    transform: translateX(0);
+  }
+
+  .container.right-panel-active .overlay-right {
+    transform: translateX(20%);
+  }
+
+  .button1 {
+    background-color: #7a3ff3;
+    color: white;
+  }
+`;
